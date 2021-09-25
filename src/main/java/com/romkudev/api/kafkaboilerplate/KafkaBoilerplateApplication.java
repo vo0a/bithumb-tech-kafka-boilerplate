14a8ce79c313f8bb1b929e5b495216d735aa0077
daromKim <vo0a195@gmail.com>
2021-09-26 04:27:38 +0900

    init project

---
+++ src/main/java/com/romkudev/api/kafkaboilerplate/KafkaBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.romkudev.api.kafkaboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBoilerplateApplication.class, args);
+	}
+
+}
